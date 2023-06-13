e80dcd75af6c21d7a7e79cc28a8ff72374d5ecf6
uimarshall <uimarshall@gmail.com>
2023-06-13 14:59:00 +0100

    Add routes and controllers:
    This includes user registration, logout, and get profile and update profile.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,8 +2,50 @@ import asyncHandler from 'express-async-handler';
 
 // asyncHandler is a function that takes an express route handler and wraps it in a way that any errors that occur inside the route handler are passed to the next() function. This way, we don't have to use try/catch blocks in our route handlers, but instead, we can use a single error handling middleware at the end of the middleware chain.
 
+// @desc Auth user & set token
+// @route POST /api/v1/users/auth
+// @access Public
+
 const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'User Authenticated' });
 });
 
-export { authUser };
+// @desc Register a new user
+// @route POST /api/v1/users/register
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User Registered' });
+});
+
+// @desc Logout a user
+// @route POST /api/v1/users/logout
+// @access Public
+
+const logOutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User Logged Out' });
+});
+
+// @desc Get user profile
+// @route GET /api/v1/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User profile' });
+});
+
+// @desc Update user profile
+// @route PUT /api/v1/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User profile updated' });
+});
+
+export {
+  authUser,
+  registerUser,
+  logOutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
-import { authUser } from '../controllers/userController.js';
-
-// @desc Auth user & set token
-// @route POST /api/v1/users/auth
-// @access Public
+import {
+  authUser,
+  logOutUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/userController.js';
 
 const router = Router();
 
 router.post('/auth', authUser);
+router.post('/register', registerUser);
+router.post('/logout', logOutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
