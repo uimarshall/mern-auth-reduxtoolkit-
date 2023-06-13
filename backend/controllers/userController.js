40d6bfd04f280d7dd14922c51e5461c5e3dbd87e
uimarshall <uimarshall@gmail.com>
2023-06-13 20:50:06 +0100

    Add logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import generateToken from '../utils/generateToken.js';
 
 // asyncHandler is a function that takes an express route handler and wraps it in a way that any errors that occur inside the route handler are passed to the next() function. This way, we don't have to use try/catch blocks in our route handlers, but instead, we can use a single error handling middleware at the end of the middleware chain.
 
-// @desc Auth user & set token
+// @desc Auth / Login user
 // @route POST /api/v1/users/auth
 // @access Public
 
@@ -70,6 +70,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const logOutUser = asyncHandler(async (req, res) => {
+  res.cookie('jwt', null, {
+    //make sure the token name (jwt) is the same as the one in the generateToken function
+    httpOnly: true,
+    expires: new Date(Date.now(0)),
+  });
   res.status(200).json({ message: 'User Logged Out' });
 });
 
