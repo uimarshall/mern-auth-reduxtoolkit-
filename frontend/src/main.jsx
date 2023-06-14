f3ea4fd82b56fa471f27e64636175b1de71fb6cf
uimarshall <uimarshall@gmail.com>
2023-06-14 12:57:39 +0100

    Add LoginPage and FormContainer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,13 @@ import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Route>
   )
 );
